15c6000d9c16a750b08a18c25a53bdbc6aff7936
Aviv Maman <awiwmaman@outlook.com>
2024-05-28 10:24:31 +0300

    start

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,11 +11,11 @@ export const metadata: Metadata = {
     'rental, property, real estate, house, apartment, condo, find rentals, find properties, find real estate, find house, find apartment, find condo, rental property, rental house, rental apartment, rental condo, real estate property, real estate house, real estate apartment, real estate condo, house rental, apartment rental, condo rental, property rental, real estate rental, rental property search, rental house search, rental apartment search, rental condo search, real estate property search, real estate house search, real estate apartment search, real estate condo search, house rental search, apartment rental search, condo rental search, property rental search, real estate rental search, rental property listings, rental house listings, rental apartment listings, rental condo listings, real estate property listings, real estate house listings, real estate apartment listings, real estate condo listings, house rental listings, apartment rental listings, condo rental listings, property rental listings, real estate rental listings',
 };
 
-interface layoutProps {
+interface RootLayoutProps {
   children: Readonly<React.ReactNode>;
 }
 
-const RootLayout: React.FC<layoutProps> = ({ children }) => {
+const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   return (
     <html lang='en'>
       <body className={inter.className}>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
-export default function Home() {
-  return <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>;
-}
+const HomePage: React.FC = () => {
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
