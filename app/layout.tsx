cddd9c0b6f1ff10429a4728c58b7769ddffe8036
Aviv Maman <awiwmaman@outlook.com>
2024-05-30 12:06:22 +0300

    put

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import '@/assets/styles/globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import AuthProvider from '@/components/AuthProvider';
 import { ToastContainer } from 'react-toastify';
+import '@/assets/styles/globals.css';
+import 'photoswipe/dist/photoswipe.css';
 import 'react-toastify/dist/ReactToastify.css';
 
 const inter = Inter({ subsets: ['latin'] });
