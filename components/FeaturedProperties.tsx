65cb1f52909fe364f584c746768adb6d0d20f8b3
Aviv Maman <awiwmaman@outlook.com>
2024-05-31 10:59:58 +0300

    Update FeaturedProperties.tsx

--- components/FeaturedProperties.tsx
+++ components/FeaturedProperties.tsx
@@ -1,19 +1,19 @@
-import { fetchProperties } from '@/utils/requests';
+import connectDB from '@/config/database';
 import FeaturedPropertyCard from './FeaturedPropertyCard';
+import PropertyModel from '@/models/Property';
 
 const FeaturedProperties: React.FC = async () => {
-  const data = await fetchProperties({
-    showFeatured: true,
-  });
+  await connectDB();
+  const properties = (await PropertyModel.find({ is_featured: true }).lean()) as any[];
 
   return (
-    data?.total &&
-    data.total > 0 && (
+    properties?.length &&
+    properties.length > 0 && (
       <section className='bg-blue-50 px-4 pt-6 pb-10'>
         <div className='container-xl lg:container m-auto'>
           <h2 className='text-3xl font-bold text-blue-500 mb-6 text-center'>Featured Properties</h2>
           <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
-            {data?.properties?.map((property) => (
+            {properties?.map((property) => (
               <FeaturedPropertyCard key={property._id} property={property} />
             ))}
           </div>
