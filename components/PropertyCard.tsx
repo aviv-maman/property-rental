5ead9691816384e8814a9e678135d2d0e2541ccf
Aviv Maman <awiwmaman@outlook.com>
2024-05-31 10:54:48 +0300

    Update PropertyCard.tsx

--- components/PropertyCard.tsx
+++ components/PropertyCard.tsx
@@ -22,11 +22,18 @@ const PropertyCard: React.FC<PropertyCardProps> = ({ property }) => {
 
   return (
     <div className='rounded-xl shadow-md relative'>
-      <Image src={property.images[0]} alt={property.images[0] || ''} height={0} width={0} sizes='100vw' className='w-full h-auto rounded-t-xl' />
+      <Image
+        src={property.images[0]}
+        alt={property.images[0] || ''}
+        height={0}
+        width={0}
+        sizes='100vw'
+        className='w-full rounded-t-xl h-[279px] object-cover'
+      />
       <div className='p-4'>
         <div className='text-left md:text-center lg:text-left mb-6'>
           <div className='text-gray-600'>{property.type}</div>
-          <h3 className='text-xl font-bold'>{property.name}</h3>
+          <h3 className='text-xl font-bold min-h-[2lvh] line-clamp-2'>{property.name}</h3>
         </div>
         <h3 className='absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right'>
           ${getRateDisplay()}
